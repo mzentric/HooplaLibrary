186e8178f3a1cb95f33ff73de9156b8dabf94404
mzentric <mzentric@users.noreply.github.com>
2024-12-29 19:31:39 -0500

    Initial Commit

---
+++ HooplaLibrary/HooplaLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HooplaLibraryApp.swift
+//  HooplaLibrary
+//
+//  Created by maria on 12/29/24.
+//
+
+import SwiftUI
+
+@main
+struct HooplaLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
